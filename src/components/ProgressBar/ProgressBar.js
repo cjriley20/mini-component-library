73a49b690662b6dd24d97a961ef6079e70d8b606
cjriley20 <20461606+cjriley20@users.noreply.github.com>
2025-06-20 12:35:16 -0400

    Add padding to large progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -10,12 +10,21 @@ const pxToRem = (px, base = 16) => `${px / base}rem`;
 const SIZES = {
   small: {
     '--height': pxToRem(8),
+    '--outer-radius': '4px',
+    '--inner-radius': '4px',
+    '--padding': '0px',
   },
   medium: {
     '--height': pxToRem(12),
+    '--outer-radius': '4px',
+    '--inner-radius': '4px',
+    '--padding': '0px',
   },
   large: {
     '--height': pxToRem(16),
+    '--outer-radius': '8px',
+    '--inner-radius': '4px',
+    '--padding': '4px',
   },
 };
 
@@ -35,8 +44,9 @@ const Base = styled.span`
 `;
 
 const Wrapper = styled(Base)`
-  height: var(--height);
-  border-radius: 4px;
+  height: calc(var(--height) + 2 * var(--padding));
+  padding: var(--padding);
+  border-radius: var(--outer-radius);
   background-color: ${COLORS.transparentGray15};
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
 `;
@@ -44,8 +54,8 @@ const Wrapper = styled(Base)`
 const Bar = styled(Base)`
   width: ${(prop) => (prop.value * 370) / 100}px;
   height: var(--height);
-  border-top-left-radius: 4px;
-  border-bottom-left-radius: 4px;
+  border-top-left-radius: var(--inner-radius);
+  border-bottom-left-radius: var(--inner-radius);
   background-color: ${COLORS.primary};
 `;
 
