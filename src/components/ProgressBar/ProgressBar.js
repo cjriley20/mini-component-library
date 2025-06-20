a184cd09ea90a0121390328bb5b6174b327ff287
cjriley20 <20461606+cjriley20@users.noreply.github.com>
2025-06-20 12:05:44 -0400

    Add initial progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,48 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const pxToRem = (px, base = 16) => `${px / base}rem`;
+
+const SIZES = {
+  small: {
+    '--height': pxToRem(8),
+  },
+  medium: {
+    '--height': pxToRem(12),
+  },
+  large: {
+    '--height': pxToRem(16),
+  },
 };
+
+const ProgressBar = ({ value, size }) => {
+  const styles = SIZES[size];
+
+  return (
+    <Wrapper style={styles} role="progressbar" aria-valuenow={value}>
+      <Bar style={styles} value={value} />
+    </Wrapper>
+  );
+};
+
+const Base = styled.span`
+  display: inline-block;
+  width: 370px;
+`;
+
+const Wrapper = styled(Base)`
+  height: var(--height);
+  border-radius: 4px;
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
+
+const Bar = styled(Base)`
+  width: ${(prop) => (prop.value * 370) / 100}px;
+  height: var(--height);
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+  background-color: ${COLORS.primary};
+`;
 
 export default ProgressBar;
