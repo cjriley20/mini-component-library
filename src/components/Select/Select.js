338ac1fefc3156a595b0a566586b96f8a483a859
cjriley20 <20461606+cjriley20@users.noreply.github.com>
2025-06-23 15:47:04 -0400

    Add initial select element

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,49 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <>
+      <VisibleSelect>
+        <Text>{displayedValue}</Text>
+        <IconWrapper>
+          <Icon id="chevron-down" size="20" strokeWidth="2" />
+        </IconWrapper>
+      </VisibleSelect>
+      <HiddenSelect value={value} onChange={onChange}>
+        {children}
+      </HiddenSelect>
+    </>
   );
 };
+
+const HiddenSelect = styled.select`
+  opacity: 0;
+`;
+
+const VisibleSelect = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  padding: 12px 16px;
+  border-radius: 8px;
+  width: fit-content;
+  color: ${COLORS.gray700};
+
+  &:focus {
+    outline: 2px solid ${COLORS.primary};
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const Text = styled.div`
+  display: inline-block;
+  font-size: 1rem;
+  margin-right: 16px;
+`;
+
+const IconWrapper = styled.div`
+  display: inline-block;
+  vertical-align: middle;
+`;
 
 export default Select;
