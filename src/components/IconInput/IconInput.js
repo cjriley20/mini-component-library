5130681ff4d99e68271cca5a6997c3a467634b82
cjriley20 <20461606+cjriley20@users.noreply.github.com>
2025-06-24 15:03:22 -0400

    Initial version for small size

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,14 +6,44 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const IconInput = ({ label, icon, width = 250, size, ...delegated }) => {
+  return (
+    <Wrapper>
+      <IconWrapper>
+        <Icon id={icon} size="16" strokeWidth="1" />
+      </IconWrapper>
+      <TextInput width={width} {...delegated}></TextInput>
+      <VisuallyHidden>{label}</VisuallyHidden>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+const TextInput = styled.input`
+  font-size: ${14 / 16}rem;
+  color: ${COLORS.gray700};
+  font-weight: 700;
+  border: none;
+  border-bottom: 1px solid ${COLORS.black};
+  padding: 4px 0px;
+  padding-left: 24px;
+  width: ${(props) => props.width}px;
+  height: 24px;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 4px;
+  left: 0;
+  color: ${COLORS.gray700};
+`;
 
 export default IconInput;
